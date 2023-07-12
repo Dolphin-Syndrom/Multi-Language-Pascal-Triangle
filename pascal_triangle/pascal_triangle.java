24e8aaa2aa47af73ee6b5404161b61493ce19e6b
Anshul Negi <96311211+anshulnegii@users.noreply.github.com>
2023-07-12 20:43:32 +0530

    Update pascal_triangle.java

--- pascal_triangle/pascal_triangle.java
+++ pascal_triangle/pascal_triangle.java
@@ -5,13 +5,13 @@ public class pascal_triangle {
         System.out.println("Enter size of the triangle: ");
         int size = sc.nextInt();
         System.out.println("Enter character ASCII value for the triangle: ");
-        char ch = sc.next().charAt(0);
+        int ch = sc.next().charAt(0);
         for (int i = 0; i < size; i++) {
             for (int j = 0; j < size-i; j++) {
                 System.out.print(" ");
             }
             for (int j = 0; j < i+1; j++) {
-                System.out.print(ch + " ");
+                System.out.print((char)ch + " ");
             }
             System.out.println();
         }
