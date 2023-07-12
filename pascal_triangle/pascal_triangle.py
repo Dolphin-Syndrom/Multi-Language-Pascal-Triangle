2da3ce9964cb9b97aa55132e414b8c16c6273f5f
Anshul Negi <96311211+anshulnegii@users.noreply.github.com>
2023-07-12 19:34:21 +0530

    Update pascal_triangle.py

--- pascal_triangle/pascal_triangle.py
+++ pascal_triangle/pascal_triangle.py
@@ -3,7 +3,7 @@ x = int(input("Enter ASCII value for symbol: "))
 for i in range(0, n):
 	for j in range(0, n-i):
 		print(end=" ")
-	for j in range(1, i+1):
+	for j in range(0, i+1):
 		ch = chr(x)
 		print(ch, end=" ")
 	print("\r")
