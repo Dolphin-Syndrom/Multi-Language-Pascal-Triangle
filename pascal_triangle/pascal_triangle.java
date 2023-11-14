4b8e6fc405969c91c5dc6af99c640d244fd16988
Dolphin-Syndrom <83578144+Dolphin-Syndrom@users.noreply.github.com>
2023-11-14 12:42:51 +0530

    Update pascal_triangle.java

--- pascal_triangle/pascal_triangle.java
+++ pascal_triangle/pascal_triangle.java
@@ -1,17 +1,18 @@
 import java.util.Scanner;
-public class pascal_triangle {
+public class pascal {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         System.out.println("Enter size of the triangle: ");
         int size = sc.nextInt();
         System.out.println("Enter character ASCII value for the triangle: ");
-        int ch = sc.next().charAt(0);
+        int ch = sc.nextInt();
         for (int i = 0; i < size; i++) {
             for (int j = 0; j < size-i; j++) {
                 System.out.print(" ");
             }
+            char chr = (char)ch;
             for (int j = 0; j < i+1; j++) {
-                System.out.print((char)ch + " ");
+                System.out.print(chr + " ");
             }
             System.out.println();
         }
